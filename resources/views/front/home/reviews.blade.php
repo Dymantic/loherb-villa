be4470891a833afbea177b172728f76523db4ad0
Michael Joyner <joyner.michael@gmail.com>
2018-12-20 11:32:06 +0800

    wip

--- resources/views/front/home/reviews.blade.php
+++ resources/views/front/home/reviews.blade.php
@@ -3,12 +3,12 @@
     <div data-flickity class="slide-show arrows-wide low-dots max-w-md mx-auto">
         @foreach($reviews as $review)
             {{--<div class="w-full m">--}}
-                <div class="w-full overflow-auto text-green-main">
+                <div class="w-full text-green-main">
                     @if($review['title'])
                         <p class="font-heading mb-4">{{ $review['title'] }}</p>
                     @endif
                     <p class="mb-4">
-                        {{ $review['content'] }}
+{{--                        {{ $review['content'] }}--}}
                     </p>
                     <p class="text-right italic"> - {{ $review['name'] }}</p>
                     <p class="text-right italic pb-16">({{ $review['source'] }})</p>
