b8c1242c1e583233d6373e35b98a7afcf54d3180
Michael Joyner <joyner.michael@gmail.com>
2018-12-20 11:27:48 +0800

    reviews structure change

--- resources/views/front/home/reviews.blade.php
+++ resources/views/front/home/reviews.blade.php
@@ -1,9 +1,9 @@
 <div class="bg-green-soft reg-section-space reviews">
     <p class="heading-text mb-16 text-green-main text-center">{{ trans('homepage.reviews.heading') }}</p>
-    <div data-flickity class="slide-show low-dots">
+    <div data-flickity class="slide-show arrows-wide low-dots max-w-md mx-auto">
         @foreach($reviews as $review)
-            <div class="w-full mx-4">
-                <div class="w-full max-w-md mx-auto overflow-auto text-green-main">
+            {{--<div class="w-full m">--}}
+                <div class="w-full overflow-auto text-green-main">
                     @if($review['title'])
                         <p class="font-heading mb-4">{{ $review['title'] }}</p>
                     @endif
@@ -14,7 +14,7 @@
                     <p class="text-right italic pb-16">({{ $review['source'] }})</p>
                     @include('front.partials.star-rating', ['rating' => $review['rating']])
                 </div>
-            </div>
+            {{--</div>--}}
         @endforeach
     </div>
 </div>
