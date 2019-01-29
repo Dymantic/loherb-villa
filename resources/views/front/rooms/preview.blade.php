3d3a09d9f968f5b2feb562f0a7a0977170b4db88
Michael Joyner <joyner.michael@gmail.com>
2019-01-29 09:24:07 +0800

    show windows in room previews

--- resources/views/front/rooms/preview.blade.php
+++ resources/views/front/rooms/preview.blade.php
@@ -1,10 +1,10 @@
-<div class="max-w-lg my-12 mx-auto relative leading-zero">
-    <picture class="w-full block">
+<div class="max-w-lg my-12 md:flex mx-auto relative leading-zero room-preview {{ $left_side ? 'left' : '' }}">
+    <picture class="w-full md:w-1/2 block">
         <source srcset="{{ $room_image_sm }}" media="(max-width: 576px)">
         <source srcset="{{ $room_image_lg }}" media="(min-width: 577px)">
         <img srcset="{{ $room_image_lg }}" alt="The {{ $room['name'] }} room">
     </picture>
-    <div class="w-full md:w-1/2 static md:absolute pin-t pin-b {{ $left_side ? 'pin-l' : 'pin-r' }} bg-green-soft md:bg-opaque flex flex-col justify-between p-4 leading-normal">
+    <div class="w-full md:w-1/2 bg-green-soft md:bg-opaque flex flex-col justify-between p-4 leading-normal">
         <div>
             <p class="heading-text text-green-main mb-4">{{ $room['name'] }}</p>
             <p class="text-green-main">{{ $room['description'] }}</p>
