bc6549e34f010b6d4603d090d22ea34bb1d9966a
Michael Joyner <joyner.michael@gmail.com>
2020-08-24 12:58:37 +0800

    padding touches

--- resources/views/components/blog-subnav.blade.php
+++ resources/views/components/blog-subnav.blade.php
@@ -1,6 +1,6 @@
-<div class="flex flex-wrap justify-around md:justify-center pb-12 pt-6 px-4 items-center bg-cream-light">
+<div class="flex flex-wrap justify-around md:justify-center pb-12 pt-6 md:px-4 items-center bg-cream-light">
     @foreach($subcategories as $category)
-        <div class="px-8 flex-shrink-0 mt-6">
+        <div class="px-4 md:px-8 flex-shrink-0 mt-6">
             <a class="{{ $isCurrent($category['slug']) ? 'underline' : 'no-underline' }} font-heading  text-green-main whitespace-no-wrap" href="{{ localUrl('journal/category/' . $category['slug']) }}">{{ $category['title'] }}</a>
         </div>
     @endforeach
