15a8f752fc3a7bdf24bf8959e2684a9deea9ab43
Michael Joyner <joyner.michael@gmail.com>
2020-08-24 12:47:37 +0800

    wip

--- resources/views/components/blog-subnav.blade.php
+++ resources/views/components/blog-subnav.blade.php
@@ -1,6 +1,6 @@
-<div class="flex justify-start md:justify-center my-6 overflow-x-auto px-4 h-12 items-center">
+<div class="flex flex-wrap justify-around md:justify-center pb-12 pt-6 px-4 items-center bg-cream-light">
     @foreach($subcategories as $category)
-        <div class="px-8 flex-shrink-0">
+        <div class="px-8 flex-shrink-0 mt-6">
             <a class="{{ $isCurrent($category['slug']) ? 'underline' : 'no-underline' }} font-heading  text-green-main whitespace-no-wrap" href="{{ localUrl('journal/category/' . $category['slug']) }}">{{ $category['title'] }}</a>
         </div>
     @endforeach
