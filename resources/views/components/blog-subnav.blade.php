daeb1d3641a32daf0fc3763b224f0f5b623dd7b7
Michael Joyner <joyner.michael@gmail.com>
2020-08-24 09:21:40 +0800

    wip

--- resources/views/components/blog-subnav.blade.php
+++ resources/views/components/blog-subnav.blade.php
@@ -1,4 +1,4 @@
-<div class="flex justify-start md:justify-center my-12 overflow-x-auto px-4">
+<div class="flex justify-start md:justify-center my-6 overflow-x-auto px-4 h-12 items-center">
     @foreach($subcategories as $category)
         <div class="px-8 flex-shrink-0">
             <a class="font-heading no-underline text-green-main whitespace-no-wrap" href="{{ localUrl('journal/category/' . $category['slug']) }}">{{ $category['title'] }}</a>
