403d466ebab517bff7991640b812cc64a2045359
Michael Joyner <joyner.michael@gmail.com>
2020-08-24 09:13:21 +0800

    blog subnav overflow

--- resources/views/components/blog-subnav.blade.php
+++ resources/views/components/blog-subnav.blade.php
@@ -1,4 +1,4 @@
-<div class="flex justify-start md:justify-center my-12 overflow-auto px-4">
+<div class="flex justify-start md:justify-center my-12 overflow-x-auto px-4">
     @foreach($subcategories as $category)
         <div class="px-8 flex-shrink-0">
             <a class="font-heading no-underline text-green-main whitespace-no-wrap" href="{{ localUrl('journal/category/' . $category['slug']) }}">{{ $category['title'] }}</a>
