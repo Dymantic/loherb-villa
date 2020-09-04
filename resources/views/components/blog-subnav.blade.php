02cfbc27055266ff2d0ce809f4730be02d1789ff
Michael Joyner <joyner.michael@gmail.com>
2020-09-04 09:48:18 +0800

    more journal stuff

--- resources/views/components/blog-subnav.blade.php
+++ resources/views/components/blog-subnav.blade.php
@@ -1,5 +1,5 @@
 <div class="pb-6 md:pb-12 pt-3 md:pt-6 md:px-4 bg-green-softest">
-    <div class="flex flex-wrap justify-around md:justify-center items-center w-9/10 sm:w-full mx-auto">
+    <div class="flex flex-wrap justify-center md:justify-center items-center w-9/10 sm:w-full mx-auto">
         @foreach($subcategories as $category)
             <div class="px-4 md:px-8 flex-shrink-0 mt-3">
                 <a class="{{ $isCurrent($category['slug']) ? 'underline' : 'no-underline' }} heading-text  text-brown-dark whitespace-no-wrap" href="{{ localUrl('journal/category/' . $category['slug']) }}">{{ $category['title'] }}</a>
--- resources/views/front/posts/index-card.blade.php
+++ resources/views/front/posts/index-card.blade.php
@@ -1,8 +1,8 @@
 <div class="flex flex-col justify-between mb-8 w-9/10 md:w-64 max-w-full bg-green-softest mx-auto">
     <div>
-        <a href="{{ localUrl('/journal/' . $post['slug']) }}" class="hidden md:block w-64 h-48">
+        <a href="{{ localUrl('/journal/' . $post['slug']) }}" class="block relative" style="padding-bottom: 75%">
             <img data-src="{{ $post['title_image_thumb'] }}"
-                 alt="{{ $post['title'] }}" class="w-full h-full object-cover lazyload">
+                 alt="{{ $post['title'] }}" class="w-full h-full object-cover absolute pin lazyload">
         </a>
         <div class="p-4">
             <p class="heading-text text-green-main">{{ $post['title'] }}</p>
