2f2509ccddeac6e96da45e068524f66033541703
Michael Joyner <joyner.michael@gmail.com>
2020-12-10 08:25:18 +0800

    up to php8

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,14 +16,11 @@ import 'lazysizes';
 window.Vue = require('vue');
 
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import BookingForm from './components/BookingForm';
+import ContactForm from './components/ContactForm';
 
-Vue.component('booking-form', require('./components/BookingForm'));
-Vue.component('contact-form', require('./components/ContactForm'));
+Vue.component('booking-form', BookingForm);
+Vue.component('contact-form', ContactForm);
 
 const app = new Vue({
     el: '#app'
