d107e860379de1ab7220de3433fc62ff821113ef
Michael Joyner <joyner.michael@gmail.com>
2021-01-11 11:19:02 +0800

    wip

---
+++ app/Accommodation/RoomTypes.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Accommodation;
+
+
+class RoomTypes
+{
+    const LIST = [
+        1 => 'economy_double',
+        2 => 'luxury_double',
+        3 => 'duplex_double',
+        4 => 'island_quad',
+        5 => 'double',
+        6 => 'premium_double',
+        7 => 'economy_quad',
+        8 => 'luxury_quad',
+        9 => 'stargazing_quad',
+    ];
+}
